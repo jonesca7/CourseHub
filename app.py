753d682fde9dfe545d75d72c0eab61a874ad8fd7
Racheal Soosaipillai <soosaipillai.racheal@gmail.com>
2020-05-03 11:05:23 -0400

    Updated search functionality to support case insensitivity and refresh button.

--- app.py
+++ app.py
@@ -18,9 +18,6 @@ db_client = MongoClient('mongodb+srv://jonesca7:tohacks2020@coursehub-8qtyk.gcp.
 db = db_client.CourseList #Create database 
 collection = db.collection #Create collection called harvard
 
-# TODO update to cluster in atlas
-# mongodb+srv://jonesca7:tohacks2020@coursehub-8qtyk.gcp.mongodb.net/test?retryWrites=true&w=majority
-
 # below is on Racheal's local machine
 app.config['MONGODB_SETTINGS'] = {
     "db": "course-hub",
@@ -32,18 +29,6 @@ initialize_routes(api)
 
 @app.route("/")
 def index():
-    # search = False
-    # q = request.args.get('q')
-    # if q:
-    #     search = True
-
-    # page = request.args.get(get_page_parameter(), type=int, default=1)
-    # # courses = collection.find()
-    # courses = collection.find().sort("name",pymongo.ASCENDING)   
-    # pagination = Pagination(page=page, total=courses.count(), search=search, record_name='courses', css_framework='bootstrap4')
-    
-    # return render_template("index.html", courses=courses, pagination=pagination)
-    
     param = request.args.get('query')
     query = {"name" : param}
 
@@ -55,7 +40,7 @@ def index():
     if param is None:
         courses = collection.find().sort("name", 1)
     else:
-        courses = collection.find({'name':{'$regex': str(param) }}).sort("name", 1)
+        courses = collection.find({'name':{'$regex': str(param), "$options" :'i' }}).sort("name", 1)
     courses_for_render = courses.limit(per_page).skip(offset)
 
     search = False
