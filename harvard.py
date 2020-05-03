311cf4f5d9f688ce7feef667b24bf7a7f579786e
CurtisAndrew Jones <jonesca7@mcmaster.ca>
2020-05-02 21:23:45 -0400

    Program now writes to database

--- harvard.py
+++ harvard.py
@@ -1,7 +1,16 @@
 from selenium.webdriver import Chrome
+from pymongo import MongoClient
 
+#Set up MongoDB client
+db_client = MongoClient('mongodb+srv://jonesca7:tohacks2020@coursehub-8qtyk.gcp.mongodb.net/test?retryWrites=true&w=majority')
+db = db_client.CourseList #Create database 
+harvard = db.harvard #Create collection called harvard
+
+#Set up Chrome driver for web browsing
 webdriver = "chromedriver.exe"
 driver = Chrome(webdriver)
+
+
 
 url = "https://online-learning.harvard.edu/catalog/free"
 driver.get(url)
@@ -9,6 +18,7 @@ driver.get(url)
 pages = driver.find_elements_by_xpath("//ul[@class='pager']/li[@class='pager-item']")
 num_pages = len(pages)
 
+course_list = []
 for page in range(num_pages + 1):
 	url = "https://online-learning.harvard.edu/catalog/Free?page=" + str(page)
 	driver.get(url)
@@ -17,4 +27,9 @@ for page in range(num_pages + 1):
 		course_title = course.find_element_by_class_name("field-name-title-qs").text
 		course_topic = course.find_element_by_class_name("field-name-subject-area").text
 		course_url = course.find_element_by_xpath("//h3/a").get_attribute("href")
-		print(course_title + " " + course_topic + " " + course_url)
+		course_object = {"name" : course_title, "topic" : course_topic, "platform" : "Harvard", "url" : course_url}
+		course_list.append(course_object)
+
+harvard.insert_many(course_list)
+
+driver.close()
